c542ee79d714c928ebdcb69b1e703f24123ce6dc
Sherklan12e <davismendoza.08@gmail.com>
2024-11-04 13:50:24 -0300

    cambios de contraseñas

--- Scripts/04 Roles.sql
+++ Scripts/04 Roles.sql
@@ -1,5 +1,5 @@
-CREATE USER IF NOT EXISTS 'Administrador'@'%' IDENTIFIED BY 'admin123';
-CREATE USER IF NOT EXISTS 'Tesorero'@'localhost' IDENTIFIED BY 'gerente123';
+CREATE USER IF NOT EXISTS 'Administrador'@'%' IDENTIFIED BY 'Admin!123';
+CREATE USER IF NOT EXISTS 'Tesorero'@'localhost' IDENTIFIED BY 'Gerente!123';
 
 
 GRANT ALL ON 5to_Ciber.* to 'Administrador'@'%';
@@ -7,8 +7,8 @@ GRANT SELECT ,DELETE ,UPDATE ON 5to_Ciber.Cuenta to 'Tesorero'@'localhost';
 GRANT SELECT, DELETE, UPDATE ON 5to_Ciber.ALquiler to 'Tesorero'@'localhost';
 GRANT SELECT, DELETE, UPDATE ON 5to_Ciber.Maquina to 'Tesorero'@'localhost';
 
-CREATE USER IF NOT EXISTS 'administrador'@'%' IDENTIFIED BY 'admin123';
-CREATE USER IF NOT EXISTS 'tesorero'@'localhost' IDENTIFIED BY 'gerente123';
+CREATE USER IF NOT EXISTS 'administrador'@'%' IDENTIFIED BY 'Admin!123';
+CREATE USER IF NOT EXISTS 'tesorero'@'localhost' IDENTIFIED BY 'Gerente!123';
 
 
 
