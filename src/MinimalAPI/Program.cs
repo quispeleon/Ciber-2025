ca3af9a000c7ec0fb81fb74632e3b31f0b6d6689
Sherklan12e <97853791+Sherklan12e@users.noreply.github.com>
2025-07-10 14:06:10 -0300

    MinimalAPi Agregado

---
+++ src/MinimalAPI/Program.cs
@@ -0,0 +1,36 @@
+using System.Data;
+using MySqlConnector;
+using Scalar.AspNetCore;
+using Ciber.Dapper;
+using Ciber.core;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+//  Obtener la cadena de conexión desde appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("MySQL");
+
+//  Registrando IDbConnection para que se inyecte como dependencia
+//  Cada vez que se inyecte, se creará una nueva instancia con la cadena de conexión
+builder.Services.AddScoped<IDbConnection>(sp => new MySqlConnection(connectionString));
+
+//Cada vez que necesite la interfaz, se va a instanciar automaticamente AdoDapper y se va a pasar al metodo de la API
+builder.Services.AddScoped<IDAO, ADOD>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "/openapi/{documentName}.json";
+    });
+    app.MapScalarApiReference();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
