84d1411269bc0aec29eaad0beb38eed1081e6211
ZupraReal <quispeleonmariabrenda@gmail.com>
2025-08-05 15:06:46 -0300

    cambio

--- src/Ciber.core/Cuenta.cs
+++ src/Ciber.core/Cuenta.cs
@@ -1,10 +1,11 @@
-
+using System.Text.Json.Serialization;
 namespace Ciber.core;
 
 public class Cuenta
 {
     public int Ncuenta { get; set; } 
     public string Nombre { get; set; }  
+     [JsonIgnore]
     public string Pass { get; set; }
     public int Dni { get; set; }
     public TimeSpan HoraRegistrada { get; set; }
